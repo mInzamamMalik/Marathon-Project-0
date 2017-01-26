4c7d9396bc93ac740fecaa4b699bfcb2b8c267b2
malikasinger1 <malikasinger1@gmail.com>
2017-01-26 22:14:33 +0500

    all users list displayed to admin

--- src/app/components/admin/user-list/user-list.component.ts
+++ src/app/components/admin/user-list/user-list.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { FireService } from '../../../providers/fire.service';
 
 @Component({
-  selector: 'app-user-list',
-  templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.css']
+    selector: 'app-user-list',
+    templateUrl: './user-list.component.html',
+    styleUrls: ['./user-list.component.css']
 })
 export class UserListComponent implements OnInit {
 
-  constructor() { }
+    constructor(private fs: FireService) { }
+    users;
 
-  ngOnInit() {
-  }
-
+    ngOnInit() {
+        this.users = this.fs
+            .getList('users')
+            .filter((user => {
+                //don't add admin in list
+                if (user.role !== "admin") {
+                    return true;
+                }
+            }));
+    }
+    deleteUser(key) {
+        console.log("key is: ", key);
+        //delete user here
+    }
 }
