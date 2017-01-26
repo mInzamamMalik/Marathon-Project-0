1f5bece982479d449c52928170895c1b72f9fc06
malikasinger1 <malikasinger1@gmail.com>
2017-01-26 22:14:19 +0500

    student list displayed to company

--- src/app/components/company/students-list/students-list.component.ts
+++ src/app/components/company/students-list/students-list.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { FireService } from '../../../providers/fire.service';
 
 @Component({
-  selector: 'app-students-list',
-  templateUrl: './students-list.component.html',
-  styleUrls: ['./students-list.component.css']
+    selector: 'app-students-list',
+    templateUrl: './students-list.component.html',
+    styleUrls: ['./students-list.component.css']
 })
 export class StudentsListComponent implements OnInit {
 
-  constructor() { }
+    constructor(private fs: FireService) { }
+    students;
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.students = this.fs
+            .getList('users')
+            .filter((user => {
+                //don't add admin in list
+                if (user.role !== "admin") {
+                    return true;
+                }
+            }));
+    }
 
 }
