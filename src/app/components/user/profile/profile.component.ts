2cda90cd6ca856eb3c485105861e67c0f99db046
malikasinger1 <malikasinger1@gmail.com>
2017-01-27 01:30:15 +0500

    user profile things completed, user can now build his resume

--- src/app/components/user/profile/profile.component.ts
+++ src/app/components/user/profile/profile.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { FireService } from '../../../providers/fire.service';
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+    selector: 'app-profile',
+    templateUrl: './profile.component.html',
+    styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+    constructor(private fs: FireService) { }
+    data = {
+        heading: "",
+        description: ""
+    }
+    auth;
+    resume;
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.fs.getUser().subscribe(auth => {
+            console.log(auth);
+            this.auth = auth;
+            this.resume = this.fs.getList("users/" + this.auth.uid + "/resume/")
 
+        });
+    }
+
+
+    add() {
+        this.fs.pushData("users/" + this.auth.uid + "/resume", this.data)
+            .catch(error => {
+                console.log("Error is: ", error);
+            })
+            .then(done => {
+                //refresh UI
+                console.log("done");               
+            })
+    }
+    remove(item) {
+        if (confirm("Do you really want to delete " + item.heading + "?"))
+            this.fs.setData("users/" + this.auth.uid + "/resume/" + item.$key, null)
+                .catch(error => {
+                    console.log("Error is: ", error);
+                })
+                .then(done => {
+                    // refresh UI
+                    console.log("removed");
+                })
+    }
 }
